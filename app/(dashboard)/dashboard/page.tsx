9695f26430e93c97ee735163ce2b06bc3d3d5a76
kuhcokrow <kuhhhckr@gmail.com>
2026-09-12 18:30:26 +0700

    feat: merge from dev rak

--- app/(dashboard)/dashboard/page.tsx
+++ app/(dashboard)/dashboard/page.tsx
@@ -1,4 +1,4 @@
-import { DashboardPage } from "@/components/pages/dashboard"
+import { DashboardPage } from "@/components/pages/dashboard/users"
 
 export default function Dashboard() {
   return <DashboardPage />
